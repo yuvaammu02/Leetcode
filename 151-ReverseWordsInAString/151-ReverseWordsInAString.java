984e74510a330c0f1153c8ff5bff8b9e3472cbf2
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:19:46 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.java
@@ -0,0 +1,14 @@
+// Last updated: 12/08/2026, 12:19:43
+class Solution {
+    public String reverseWords(String s) {
+        String[] str = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length-1;i>=0;i--){
+                sb.append(str[i]);
+                if(i!= 0){
+                    sb.append(" ");
+                }
+        }
+        return sb.toString();
+    }
+}
