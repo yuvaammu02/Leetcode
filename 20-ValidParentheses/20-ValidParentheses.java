6f9023c1d9e710e8757d7803c584b63df1ef8b0d
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:26:10 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,19 @@
+// Last updated: 12/08/2026, 12:26:07
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else{
+               if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if(c == ')' && top != '(') return false;
+                if(c == '}' && top != '{') return false;
+                if(c == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty(); 
+    }
+}
