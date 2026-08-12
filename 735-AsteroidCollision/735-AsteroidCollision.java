bd49159f39b5dd9e45c62280a1009087601e922a
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:02:57 +0530

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.java
@@ -0,0 +1,27 @@
+// Last updated: 12/08/2026, 12:02:54
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();   
+        for (int asteroid : asteroids) {
+            boolean alive = true;
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+                if (stack.peek() < -asteroid) {
+                    stack.pop();
+                    continue;
+                } else if (stack.peek() == -asteroid) {
+                    stack.pop();
+                }
+                alive = false;
+                break;
+            }
+            if (alive) {
+                stack.push(asteroid);
+            }
+        }
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; --i) {
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
