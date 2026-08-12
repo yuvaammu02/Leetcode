78cd96408046d278bda62eda4b1ad0a9cfaa007a
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:12:15 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,14 @@
+// Last updated: 12/08/2026, 12:12:12
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n+1];
+        int sub =1;
+        for(int i = 1;i<=n;i++){
+            if(sub*2 == i){
+                sub = i;
+            }
+            dp[i] = dp[i-sub] + 1;
+        }
+        return dp;
+    }
+}
