ba9d7f7908059ad19498b159653caa392aaadf75
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 11:56:36 +0530

    Create 1480-MovieRating

---
+++ 1480-MovieRating/1480-MovieRating.sql
@@ -0,0 +1,21 @@
+-- Last updated: 12/08/2026, 11:56:33
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr
+    ON u.user_id = mr.user_id
+    GROUP BY u.user_id, u.name
+    ORDER BY COUNT(*) DESC, u.name ASC
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr
+    ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id, m.title
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+);
