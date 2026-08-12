47ae026a63ca1f7e595b91c7bcf2762dba0aada0
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:13:07 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 12/08/2026, 12:13:02
+class Solution {
+    public boolean canWinNim(int n) {
+        if( n%4 != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
