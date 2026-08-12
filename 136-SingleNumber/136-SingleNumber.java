ae090d8505e3d9332ac64a4ae668301e211cfeef
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:20:43 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 12/08/2026, 12:20:39
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result =0;
+        for(int num : nums){
+            result ^= num;
+        }
+        return result;
+    }
+}
