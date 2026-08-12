e94b4fd7fc7a9a174a63448d83de7d6e91c291b8
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:03:38 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,13 @@
+// Last updated: 12/08/2026, 12:03:35
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x =0,y=0;
+        for(char m : moves.toCharArray()){
+            if(m == 'R') x++;
+            else if(m == 'L') x--;
+            else if(m == 'U') y++;
+            else if(m == 'D') y--;
+        }
+        return x == 0 && y == 0;
+    }
+}
