fc639610657e050672d5213f3c003260131edada
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 11:52:57 +0530

    Create 3276-MinimumNumberOfPushesToTypeWordIi

---
+++ 3276-MinimumNumberOfPushesToTypeWordIi/3276-MinimumNumberOfPushesToTypeWordIi.java
@@ -0,0 +1,13 @@
+// Last updated: 12/08/2026, 11:52:54
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) 
+            freq[c - 'a']++;
+        Arrays.sort(freq);
+        int ans = 0;
+        for (int i = 25; i >= 0 && freq[i] > 0; i--) 
+            ans += freq[i] * ((25 - i) / 8 + 1);
+        return ans;
+    }
+}
