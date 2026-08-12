bf496b689f2ddaed372b4bdb6f41bee922f60a82
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:02:09 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,20 @@
+// Last updated: 12/08/2026, 12:02:06
+class Solution {
+    public int search(int[] nums, int target) {
+        int left =0;
+        int right = nums.length-1;
+        while(left <= right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid +1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+    return -1;    
+    }
+}
