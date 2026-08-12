0ca23b8211e60933b7e347f476dd5f78043e1c83
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:12:23 +0530

    Create 334-IncreasingTripletSubsequence

---
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.java
@@ -0,0 +1,13 @@
+// Last updated: 12/08/2026, 12:12:21
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for(int num : nums){
+            if(num <= min1) min1 = num;
+            else if(num <= min2) min2 = num;
+            else return true;
+        }
+        return false;
+    }
+}
