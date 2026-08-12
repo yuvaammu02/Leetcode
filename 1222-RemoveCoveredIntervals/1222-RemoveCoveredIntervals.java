80f96c7e4f0ae75404ef2246ffc995688f0ac535
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 11:58:13 +0530

    Create 1222-RemoveCoveredIntervals

---
+++ 1222-RemoveCoveredIntervals/1222-RemoveCoveredIntervals.java
@@ -0,0 +1,17 @@
+// Last updated: 12/08/2026, 11:58:10
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) ->
+            a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]
+        );
+        int count = 0;
+        int maxEnd = 0;
+        for (int[] interval : intervals) {
+            if (interval[1] > maxEnd) {
+                count++;
+                maxEnd = interval[1];
+            }
+        }
+        return count;
+    }
+}
