a02530ae525b1dde71deee7d1849840691a53fd9
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:15:35 +0530

    Create 224-BasicCalculator

---
+++ 224-BasicCalculator/224-BasicCalculator.java
@@ -0,0 +1,44 @@
+// Last updated: 12/08/2026, 12:15:32
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        int number = 0;
+        int sign = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                number = number * 10 + (ch - '0');
+            } 
+            else if (ch == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } 
+            else if (ch == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } 
+            else if (ch == '(') {
+                stack.push(result);
+                stack.push(sign);
+
+                result = 0;
+                sign = 1;
+            } 
+            else if (ch == ')') {
+                result += sign * number;
+                number = 0;
+
+                result *= stack.pop(); // Previous sign
+                result += stack.pop(); // Previous result
+            }
+        }
+
+        result += sign * number;
+        return result;
+    }
+}
