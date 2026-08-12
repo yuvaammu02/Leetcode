afbb2292e4db36c814baf976156405cb9aded9d5
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:15:26 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.java
@@ -0,0 +1,33 @@
+// Last updated: 12/08/2026, 12:15:23
+class MyStack {
+       Stack<Integer> stack;
+    public MyStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+    }
+    
+    public int pop() {
+       int x = stack.pop();
+        return x;
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
