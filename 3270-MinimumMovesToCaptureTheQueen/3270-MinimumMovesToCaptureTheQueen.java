5065b3f5f4eb3fd079f06f4a157fedcb59da914c
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 11:53:02 +0530

    Create 3270-MinimumMovesToCaptureTheQueen

---
+++ 3270-MinimumMovesToCaptureTheQueen/3270-MinimumMovesToCaptureTheQueen.java
@@ -0,0 +1,15 @@
+// Last updated: 12/08/2026, 11:52:59
+class Solution {
+    public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        if(a == e || b == f) {
+            if(a == e && a == c && ((d-b) * (d-f) < 0)) return 2;
+            if(b == f && b == d && ((c-a) * (c-e) < 0)) return 2;
+            return 1;
+        }
+        if(Math.abs(c - e) == Math.abs(d - f)) {
+            if(Math.abs(c - a) == Math.abs(d - b) && Math.abs(e - a) == Math.abs(f - b) && ((b-f) * (b-d) < 0))  return 2;
+            return 1;
+        }
+        return 2;
+    }
+}
