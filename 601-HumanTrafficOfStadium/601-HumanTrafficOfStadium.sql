3e169654cc70415ea85ebab6d852f2d758291f1f
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 12:04:54 +0530

    Create 601-HumanTrafficOfStadium

---
+++ 601-HumanTrafficOfStadium/601-HumanTrafficOfStadium.sql
@@ -0,0 +1,11 @@
+-- Last updated: 12/08/2026, 12:04:51
+# Write your MySQL query statement below
+with q1 as (
+select *, id - row_number() over() as id_diff
+from stadium
+where people > 99
+)
+select id, visit_date, people
+from q1
+where id_diff in (select id_diff from q1 group by id_diff having count(*) > 2)
+order by visit_date
