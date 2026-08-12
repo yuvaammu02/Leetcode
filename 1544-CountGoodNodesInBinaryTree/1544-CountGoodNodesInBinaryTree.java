952e602e945a1632d6481faf0bca03ca0c9a7b89
YUVASRI C <yuvaammu02@gmail.com>
2026-08-12 11:56:13 +0530

    Create 1544-CountGoodNodesInBinaryTree

---
+++ 1544-CountGoodNodesInBinaryTree/1544-CountGoodNodesInBinaryTree.java
@@ -0,0 +1,34 @@
+// Last updated: 12/08/2026, 11:56:11
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return dfs(root,root.val);
+    }
+    private int dfs(TreeNode root,int max){
+        if(root == null){
+            return 0;
+        }
+        int count =0;
+        if(root.val >= max){
+            count++;
+        }
+        max = Math.max(max,root.val);
+        count += dfs(root.left,max);
+        count += dfs(root.right,max);
+    return count;
+    }
+}
